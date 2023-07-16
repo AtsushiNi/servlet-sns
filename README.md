a0e0819f8250ea57aa79ba76509cf25bdbfd7c31
AtsushiNi <atsushi9731@gmail.com>
2023-07-16 15:45:19 +0900

    design

---
+++ README.md
@@ -0,0 +1,6 @@
+# H2 consoleの起動方法
+ルートディレクトリに移動して以下を実行
+```
+java -jar src/main/webapp/WEB-INF/lib/h2-2.2.220.jar
+```
+url:jdbc:h2:tcp://localhost/./src/main/webapp/WEB-INF/lib/test
---
+++ src/main/java/controllers/FrontController.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import actions.Action;
+
+@WebServlet(urlPatterns = {"*.action"})
+public class FrontController extends HttpServlet {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		
+		try {
+			String path = request.getServletPath().substring(5);
+			String name = "webapp.WEB-INF.actions." + path.replace(".action", "Action").replace('/', '.');
+			Action action = (Action)Class.forName(name).getDeclaredConstructor(null).newInstance();
+			String url = action.execute(request, response);
+			request.getRequestDispatcher(url).forward(request, response);
+		} catch(Exception e) {
+			e.printStackTrace(out);
+		}
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+}
